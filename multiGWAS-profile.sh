21da052deefd663100a5b4845a761aa99991627e
Luis Garreta - Agrosavia <lgarreta@agrosavia.co>
2020-05-10 16:53:26 -0500

    MultiJWAS 1.0: CLI and GUI

---
+++ multiGWAS-profile.sh
@@ -0,0 +1,5 @@
+#------------------- multiGWAS HOME ---------------------
+export MULTIGWAS_HOME=/home/lg/agrosavia/development/multiGWAS
+MULTIGWAS_TOOLS=$MULTIGWAS_HOME/tools
+MULTIGWAS_SOURCES=$MULTIGWAS_HOME/sources
+export PATH=$PATH:$MULTIGWAS_TOOLS:$MULTIGWAS_SOURCES
