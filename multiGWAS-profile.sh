43e64cf4bf5e946a0d59ff7ef47cd3349b248282
Luis Garreta <lgarreta@agrosavia.co>
2020-07-24 10:55:27 -0500

    Added docs-supplements

---
+++ multiGWAS-profile.sh
@@ -0,0 +1,5 @@
+#------------------- multiGWAS HOME ---------------------
+export MULTIGWAS_HOME=/home/lg/agrosavia/GWAS/MultiGWAS
+MULTIGWAS_TOOLS=$MULTIGWAS_HOME/tools
+MULTIGWAS_SOURCES=$MULTIGWAS_HOME/sources
+export PATH=$PATH:$MULTIGWAS_TOOLS:$MULTIGWAS_SOURCES
