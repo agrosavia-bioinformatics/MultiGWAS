a5a615193084c158a66825343f47d2493443b400
Luis Garreta <lgarreta@agrosavia.co>
2020-08-19 14:17:15 -0500

    VCF2

---
+++ multiGWAS-profile.sh
@@ -0,0 +1,6 @@
+
+#------------------- multiGWAS.R profile ---------------------
+export MULTIGWAS_HOME=/home/lg/agrosavia/GWAS/MultiGWAS
+MULTIGWAS_TOOLS=$MULTIGWAS_HOME/tools
+MULTIGWAS_SOURCES=$MULTIGWAS_HOME/sources
+export PATH=$PATH:$MULTIGWAS_TOOLS:$MULTIGWAS_SOURCES
