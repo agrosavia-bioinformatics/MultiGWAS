6d1661b81bae08004766c82404f8751529aff601
Luis Garreta <lgarreta@agrosavia.co>
2020-08-21 17:18:11 -0500

    Mdf Install, Add Uninstall

--- multiGWAS_profile.sh
+++
@@ -1,7 +0,0 @@
-
-#------------------- multiGWAS.R profile ---------------------
-echo "...multiGWAS profile"
-export MULTIGWAS_HOME=/home/lg/agrosavia/GWAS/MultiGWAS
-MULTIGWAS_TOOLS=$MULTIGWAS_HOME/tools
-MULTIGWAS_SOURCES=$MULTIGWAS_HOME/sources
-export PATH=$PATH:$MULTIGWAS_TOOLS:$MULTIGWAS_SOURCES
